b7690e6d03191c135f5f90ea3d7665c8a5f5fe0a
DawidHerc <dawidherc@gmail.com>
2023-11-24 08:16:21 +0100

    Removing unneeded tests

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -3,35 +3,6 @@
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 
-test('login screen can be rendered', function () {
-    $response = $this->get('/login');
-
-    $response->assertStatus(200);
-});
-
-test('users can authenticate using the login screen', function () {
-    $user = User::factory()->create();
-
-    $response = $this->post('/login', [
-        'email' => $user->email,
-        'password' => 'password',
-    ]);
-
-    $this->assertAuthenticated();
-    $response->assertRedirect(RouteServiceProvider::HOME);
-});
-
-test('users can not authenticate with invalid password', function () {
-    $user = User::factory()->create();
-
-    $this->post('/login', [
-        'email' => $user->email,
-        'password' => 'wrong-password',
-    ]);
-
-    $this->assertGuest();
-});
-
 test('users can authenticate through api', function () {
     $user = User::factory()->create();
 
